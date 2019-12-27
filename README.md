49dbe807129d90502368d8029d286e142162425d
Navdeep Singh Rathore <navdeep@Navdeeps-MacBook-Pro.local>
2019-12-27 12:44:55 +0530

    instance variables

--- README.md
+++ README.md
@@ -10,4 +10,7 @@
 * [Data Types : Primitive and Non-primitive](dataTypes/DataTypes_01.java)
 * [Print Default values of both Primitive and Non-primitive data types](dataTypes/DataTypes_02.java)
 ### 3) Variables
+* [Instance Variable](variables/Var_01.java)
+* [Static Variable](variables/Var_02.java)
+* [Local Variable](variables/Var_03.java)
 ### 4) OOPS {Object Oriented Programming}
---
+++ variables/Var_01.java
@@ -0,0 +1,35 @@
+/**
+ * @author : Navdeep
+ * Variable Program 01: Instance Variables
+ * Here we will discuss about one of the Variable type which is Instance Variables available in Java
+ */
+public class Var_01 {
+
+    /*
+     * NOTES:
+     * Instance variables are non-static variables and are declared in a class outside any method, constructor or block
+     * a) As instance variables are declared in a class, these variables are created when an object of the class is created and destroyed when the object is destroyed.
+     * b) Unlike local variables, we may use access specifiers for instance variables. If we do not specify any access specifier then the default access specifier will be used.
+     * c) Initialization of Instance Variable is not Mandatory. Its default value is 0
+     * d) Instance Variable can be accessed only by creating objects.
+     */
+    int x = 10;
+    int y;
+
+    void display(){
+        y = 20;
+        System.out.println(x);  //10
+        System.out.println(y);  //20
+    }
+    public static void main(String[] args) {
+        // System.out.println(x);   //Invalid, can't use instance variable directly in static method
+        // System.out.println(Var_01.x); //Invalid, can't use class name either
+        
+        Var_01 obj = new Var_01();
+        System.out.println(obj.x);  //10
+        obj.display();
+        System.out.println(obj.y);  //20
+        obj.y = 30;
+        System.out.println(obj.y);  //30
+    }
+}
